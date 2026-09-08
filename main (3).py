e26c793a5536240494095447a77af73ed307c8fd
Keith Macharia <keithmacharia0@gmail.com>
2026-09-08 11:26:46 +0300

    Remove notice from main.py

--- main (3).py
+++ main (3).py
@@ -1,6 +1,5 @@
 """People Counter."""
 """
- Copyright (c) 2018 Intel Corporation.
  Permission is hereby granted, free of charge, to any person obtaining
  a copy of this software and associated documentation files (the
  "Software"), to deal in the Software without restriction, including
